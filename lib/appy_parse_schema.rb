f509b910bc4c08662c3ced496ce330f3b03bc9e1
James Becker <jamsbec@gmail.com>
2018-02-22 14:55:23 +0700

    finish basic rspec

--- lib/appy_parse_schema.rb
+++ lib/appy_parse_schema.rb
@@ -1,27 +1,65 @@
-# This is the schema for output of appy_parse
-
 {
-  {
-    'web.11' => {
-      '70.115.253.79' => 1,
-      '70.115.253.20' => 2
+    "camera_ip_calls_by_home" => {
+        "web.13" => {
+                 "5.13.87.91" => 1,
+              "121.7.211.216" => 1,
+              "110.50.171.35" => 1,
+            "166.147.102.240" => 1
+        },
+        "web.12" => {
+                "99.138.1.64" => 1,
+             "216.161.91.207" => 1,
+              "99.199.86.214" => 1
+        }
     },
-    'web.6' => {
-      '70.115.253.30' => 3,
-      '70.115.253.1' => 1
-    }
-  },
-  {
-    'GET /api/users/{user_id}/get_camera' =>
-  }
+             "response_times" => {
+             "get_camera" => {
+              "mean" => 228,
+            "median" => 143.0,
+              "mode" => 67
+        },
+               "get_home" => {
+              "mean" => 62,
+            "median" => 32.0,
+              "mode" => 23
+        },
+        "get_all_cameras" => {
+              "mean" => 25,
+            "median" => 15.0,
+              "mode" => 11
+        },
+           "POST_user_id" => {
+              "mean" => 82,
+            "median" => 46.0,
+              "mode" => 23
+        },
+            "GET_user_id" => {
+              "mean" => nil,
+            "median" => nil,
+              "mode" => nil
+        }
+    },
+      "service_times_ranking" => [
+        [   0] {
+                       "ip" => "99.107.248.149",
+                       "id" => "web.1",
+            "response_time" => 37
+        },
+        [   1] {
+                       "ip" => "98.193.249.146",
+                       "id" => "web.11",
+            "response_time" => 38
+        },
+        [1531] {
+                       "ip" => "172.250.230.38",
+                       "id" => "web.4",
+            "response_time" => 3156
+        },
+        [1532] {
+                       "ip" => "39.229.227.190",
+                       "id" => "web.6",
+            "response_time" => 6626
+        }
+    ],
+          "entries_processed" => 6637
 }
-
-
-
-
-# Average response times:
-# 'GET /api/users/{user_id}/get_camera'
-# GET /api/users/{user_id}/get_home
-# GET /api/users/{user_id}/get_all_cameras
-# POST /api/users/{user_id}
-# GET /api/users/{user_id}
--- spec/lib/appy_parse_spec.rb
+++ spec/lib/appy_parse_spec.rb
@@ -4,12 +4,23 @@ require "appy_parse.rb"
 
 describe AppyParse do
   ap = AppyParse.new
-  it 'returns a string' do
-    file = 'sample_appysphere.log'
-    output = ap.parseIO(file)
 
+  file = 'sample_appysphere.log'
+  output = ap.parseIO(file)
 
+  it 'returns a hash' do
     expect(output).to be_a(Hash)
-
   end
+
+  it 'returns four correct objects' do
+    expect(output).to have_key('camera_ip_calls_by_home')
+    expect(output).to have_key('response_times')
+    expect(output).to have_key('service_times_ranking')
+    expect(output).to have_key('entries_processed')
+  end
+
+  it 'returns the correct number of entries processed' do
+    expect(output['entries_processed']).to equal(6637)
+  end
+
 end
